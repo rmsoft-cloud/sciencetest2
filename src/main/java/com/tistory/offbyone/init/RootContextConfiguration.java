83d8ca14bd9965ce6d774db6f204c568d7e2a900
Kal1125 <kjh@rmsoft.kr>
2024-05-21 09:31:49 +0900

    test

---
+++ src/main/java/com/tistory/offbyone/init/RootContextConfiguration.java
@@ -0,0 +1,12 @@
+package com.tistory.offbyone.init;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 루트 컨텍스트 설정파일
+ * @author PENTODE
+ *
+ */
+@Configuration
+public class RootContextConfiguration {
+}
